aece4387579300b1ae5bbb74880bd94ecefac680
krrong <khk9664@naver.com>
2021-11-05 22:09:23 +0900

    basic tree

---
+++ Algorithm/red_black tree.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#define RED 1
+#define BLACK 0
+using namespace std;
+
+// 노드 -> id, 이름, 용량, 가격, 컬러, left_child, right_child, parent 저장
+class Node {
+public:
+	int id;
+	string name;
+	int file_size;
+	int price;
+	int color;
+	Node* parent;		// 부모 링크
+	Node* left_child;	// 왼쪽 자식 링크
+	Node* right_child;	// 오른쪽 자식 링크
+
+	// 생성자
+	Node(int i, string n, int fs, int p) {
+		id = i;
+		name = n;
+		file_size = fs;
+		price = p;
+		color = RED;
+		parent = left_child = right_child = NULL;
+	}
+
+	// 왼쪽 자식 설정
+	void setLeft(Node* node) {
+		if (node == NULL) {
+			this->left_child = NULL;
+		}
+		else {
+			this->left_child = node;
+			node->parent = this;
+		}
+	}
+
+	// 오른쪽 자식 설정
+	void setRight(Node* node) {
+		if (node == NULL) {
+			this->right_child = NULL;
+		}
+		else {
+			this->right_child = node;
+			node->parent = this;
+		}
+	}
+
+	friend class RedBlackTree;
+};
+
+class RedBlackTree {
+public:
+	Node* root;
+
+	// 생성자
+	RedBlackTree() {
+		root = NULL;
+	}
+
+	// 검색
+	Node* search(int input_id) {
+		Node* curNode = root;
+		int depth = -1;			// 깊이
+		while (curNode != NULL) {
+			depth++;
+			// 현재 노드의 id와 찾는 id가 같을 경우
+			if (curNode->id == input_id) {
+				cout << depth << ' ';
+				return curNode;
+			}
+			// 현재 노드의 id보다 찾는 id가 클 경우 -> 오른쪽으로 이동
+			else if (curNode->id < input_id) {
+				curNode = curNode->right_child;
+			}
+			// 현재 노드의 id보다 찾는 id가 작을경우 -> 왼쪽으로 이동
+			else {
+				curNode = curNode->left_child;
+			}
+		}
+		// 없으면 NULL리턴
+		return NULL;
+	}
+
+	int insert(Node* node) {
+		// 트리가 비어있다면
+		if (root == NULL) {
+			root = node;
+			return 0;
+		}
+		
+		Node* parNode = NULL;
+		Node* curNode = root;
+		int depth = 0;
+
+		while (curNode != NULL) {
+			depth++;
+			// 새로 입력하는 id가 더 큰 경우
+			if (curNode->id < node->id) {
+				parNode = curNode;
+				curNode = curNode->right_child;
+			}
+			// 새로 입력하는 id가 더 작은 경우
+			else {
+				parNode = curNode;
+				curNode = curNode->left_child;
+			}
+		}
+
+		if (parNode->id > node->id) {
+			parNode->setLeft(node);
+		}
+		else if (parNode->id < node->id) {
+			parNode->setRight(node);
+		}
+		return depth;
+	}
+
+	void update(int id, string name, int file_size, int price) {
+		Node* node = search(id);
+
+		// 업데이트 하려는 노드가 없을 경우
+		if (node == NULL) {
+			cout << "NULL";
+		}
+		
+		else {
+			node->name = name;
+			node->file_size = file_size;
+			node->price = price;
+		}
+		cout << '\n';
+	}
+
+	void discount(int x, int y, int p) {
+
+	}
+};
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	RedBlackTree tree;
+	int t, id, file_size, price;
+	string name, query;
+
+	cin >> t;
+	while (t--) {
+		cin >> query;
+		// 등록 (insert)
+		if (query == "I") {
+			cin >> id >> name >> file_size >> price;
+			Node* newNode = new Node(id, name, file_size, price);
+
+			Node* node = tree.search(id);
+			
+			// 없을 경우 추가
+			if (node == NULL) {
+				cout << tree.insert(newNode);
+			}
+			cout << '\n';
+		}
+		// 검색 (search)
+		else if (query == "F") {
+			cin >> id;
+			Node* node = tree.search(id);
+
+			if (node == NULL) {
+				cout << "NULL" << '\n';
+			}
+			else {
+				cout << node->name << ' ' << node->file_size << ' ' << node->price << '\n';
+			}
+		}
+		// 업데이트 (update)
+		else if (query == "R") {
+			cin >> id >> name >> file_size >> price;
+			tree.update(id, name, file_size, price);
+		}
+		// 할인 (discount)
+		else if (query == "D") {
+			int x, y, p;
+			cin >> x >> y >> p;
+			tree.discount(x, y, p);
+		}
+	}
+	return 0;
+}
